1844ed00b9214126c8c30661b602488afe3621ea
Hasinur3813 <hasinur.rahman1332@gmail.com>
2024-04-24 10:45:29 +0600

    'first'

---
+++ src/js/main.js
@@ -0,0 +1,7 @@
+'use strict';
+const mobileMenu = document.getElementById('mobileMenu');
+const menuIcon = document.getElementById('menuIcon');
+
+function openMenu(){
+    mobileMenu.classList.toggle('hidden');
+}
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      colors: {
+        "body-bg": "#F6FDFF",
+        "primary-color": "#1D7293",
+        "text-color": "#073141",
+        "bg-secondary": "#B9D6E1",
+      },
+      container: {
+        center: "true",
+      },
+    },
+  },
+  plugins: [],
+};
