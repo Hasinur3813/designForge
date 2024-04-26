4df4bb9305f15e0a5fa81e0d596d3211c28c93ef
Hasinur3813 <hasinur.rahman1332@gmail.com>
2024-04-26 22:55:39 +0600

    sections added

--- src/js/main.js
+++ src/js/main.js
@@ -5,3 +5,18 @@ const menuIcon = document.getElementById('menuIcon');
 function openMenu(){
     mobileMenu.classList.toggle('hidden');
 }
+
+
+
+function sortNum(n){
+    let nArr = n.split(',')
+    let sortedNumber;
+    sortedNumber = nArr.sort((a,b)=>{
+        return a-b;
+    });
+    return sortedNumber;
+}
+
+
+
+console.log(sortNum('2,3,5,3,33,23,64,6,7'));
