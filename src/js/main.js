74ffa7f8c4bb2909525834d1416f036445aa5e0a
Hasinur3813 <hasinur.rahman1332@gmail.com>
2024-04-28 22:36:05 +0600

    update

--- src/js/main.js
+++ src/js/main.js
@@ -1,22 +1,24 @@
-'use strict';
-const mobileMenu = document.getElementById('mobileMenu');
-const menuIcon = document.getElementById('menuIcon');
+"use strict";
+const mobileMenu = document.getElementById("mobileMenu");
+const menuIcon = document.getElementById("menuIcon");
 
-function openMenu(){
-    mobileMenu.classList.toggle('hidden');
+function openMenu() {
+  mobileMenu.classList.toggle("hidden");
 }
 
-
-
-function sortNum(n){
-    let nArr = n.split(',')
-    let sortedNumber;
-    sortedNumber = nArr.sort((a,b)=>{
-        return a-b;
-    });
-    return sortedNumber;
-}
-
-
-
-console.log(sortNum('2,3,5,3,33,23,64,6,7'));
+const swiper = new Swiper(".testimonials_slider", {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  centeredSlides: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  loop: true,
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+  },
+});
